734f4e0df400f38849d0a670c0b93b7a77af3e5a
Toby Akabogu <tobeakabogu@gmail.com>
2020-07-21 23:11:00 -0500

    More frontend homepage changes

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,10 +3,11 @@ import { FC } from "react";
 import LayoutStyle from "../components/LayoutStyle";
 import App from "../components/App";
 import RestaurantList from "../components/RestaurantList";
-import { Layout, Input, Card, Row, Col} from "antd";
+import { Layout, Input, Card, Row, Col, Typography} from "antd";
 import FooterLayout from "../components/FooterLayout";
 
 const { Search } = Input;
+const { Title } = Typography;
 
 const { Content } = Layout;
 const IndexPage: FC = () => (
@@ -26,21 +27,31 @@ const IndexPage: FC = () => (
             style={{fontSize: 18}}
           />
         </div>
+        <div style={{marginTop: 50, textAlign: "center"}}>
+          <Title>Restaurant Review</Title>
+          <Title level={4}>Hello Food Critics</Title>
+        </div>
         <div>
-          <Row gutter={16}>
+          <Row justify="space-around" style={{marginTop: 50, marginBottom: 50}}>
             <Col>
-              <Card bordered={true} bodyStyle={{}}>
+              <Card title="Default Title" bordered={true} style={{width: 400}}>
                 <p><RestaurantList /></p>
+                <p>Card Content</p>
+                <p>Card Content</p>
               </Card>
             </Col>
             <Col>
-              <Card bordered={true}>
-                Card content
+              <Card title="Default Title" bordered={true} style={{width: 400}}>
+                <p>Card Content</p>
+                <p>Card Content</p>
+                <p>Card Content</p>
               </Card>
             </Col>
             <Col>
-              <Card bordered={true}>
-                Card content
+              <Card title="Default Title" bordered={true} style={{width: 400}}>
+                <p>Card Content</p>
+                <p>Card Content</p>
+                <p>Card Content</p>
               </Card>
             </Col>
           </Row>
